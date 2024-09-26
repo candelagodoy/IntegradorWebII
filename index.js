032ac44435ad9fe73901646eda2b0f005ad40c44
Candela Guadalupe Godoy <163220967+candelagodoy@users.noreply.github.com>
2024-09-26 17:45:53 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const pug = require('pug');
-//const cors = require('cors');
 const path = require('path');
 
 const app = express();
@@ -11,7 +10,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
-//app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/',require('./routes/home'));
 
